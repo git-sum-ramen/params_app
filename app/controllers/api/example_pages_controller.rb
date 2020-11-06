bc13ece5b9b13026d81531d7af91b93cc41b2143
brian <riceb53@gmail.com>
2020-11-05 19:20:24 -0500

    add additional query params examples

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,7 +1,12 @@
 class Api::ExamplePagesController < ApplicationController
   def query
     p "here is the params hash"
-    p params["name"]
+    @occupation = params["occupation"].length
+    if @occupation > 12
+      @message = "you have a fancy sounding job"
+    else
+      @message = "you have a regular sounding job"
+    end
     render 'query.json.jb'
   end
 end
