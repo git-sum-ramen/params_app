b144eaf9d92b154180eaa59fd6be98ebb9f0ca3b
brian <riceb53@gmail.com>
2020-11-05 20:36:24 -0500

    name exercise

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,5 +1,12 @@
 class Api::GamesController < ApplicationController
   def tell_name
+    @name = params[:name].upcase
+    p @name
+    if @name[0] == "A"
+      @message = "Hey, your name starts with the first letter of the alphabet!"
+    else
+      @message = ""
+    end
     render 'name.json.jb'
   end
 end
