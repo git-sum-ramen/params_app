f2835305413a230a104bb2d43b44c03658f57778
brian <riceb53@gmail.com>
2020-11-05 21:20:57 -0500

    add url segment parameter

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -9,4 +9,10 @@ class Api::ExamplePagesController < ApplicationController
     end
     render 'query.json.jb'
   end
+
+  def segment
+    @vehicle = params[:vehicle] 
+    @sentence = "You drive a #{@vehicle}"   
+    render 'segment.json.jb'
+  end
 end
